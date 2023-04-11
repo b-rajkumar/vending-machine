efce0268e1799769ac456e7f6d654b6d1c1f2b48
RAJKUMAR BUDDHA <rajkumarbuddha@RAJKUMARs-MacBook-Pro.local>
2023-04-11 17:06:48 +0530

    Upgraded vending machine to dispense optimum number of 5,2 and 1 rupee coins

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,16 +1,33 @@
-const numberOfTwoRupeeCoins = function(amount) {
-  return Math.trunc(amount/2);
-}
-
 const numberOfOneRupeeCoins = function(amount) {
   return amount;
 }
 
-const dispenseCoins = function(amount) {
-  let totalCoins = numberOfTwoRupeeCoins(amount);
+const numberOfTwoRupeeCoins = function(amount) {
+  return Math.trunc(amount/2);
+}
 
-  remainingAmount = amount - (2 * totalCoins);
-  totalCoins += numberOfOneRupeeCoins(remainingAmount);
+const numberOfFiveRupeeCoins = function(amount) {
+  return Math.trunc(amount/5);
+}
+
+const calculateRemainingAmount = function(amount, coinValue, numberOfCoins) {
+  return amount - (coinValue * numberOfCoins);
+}
+
+const dispenseCoins = function(amount) {
+  let totalCoins = 0;
+
+
+  const fiveRupeeCoins = numberOfFiveRupeeCoins(amount);
+  let remainingAmount = calculateRemainingAmount(amount, 5, fiveRupeeCoins); 
+  totalCoins += fiveRupeeCoins; 
+
+  const twoRupeeCoins = numberOfTwoRupeeCoins(remainingAmount);
+  remainingAmount = calculateRemainingAmount(remainingAmount, 2, twoRupeeCoins); 
+  totalCoins += twoRupeeCoins; 
+
+  totalCoins += numberOfOneRupeeCoins(remainingAmount); 
+
 
   return totalCoins;
 }
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -5,7 +5,9 @@ const testVendingMachine = function() {
   test.assertEquality(0, vendingMachine.dispenseCoins(0), '0 rupees amount should give nothing');
   test.assertEquality(1, vendingMachine.dispenseCoins(1), '1 rupees amount should give 1 coin');
   test.assertEquality(1, vendingMachine.dispenseCoins(2), '2 rupees amount should give 1 coins');
-  test.assertEquality(23, vendingMachine.dispenseCoins(45) , '45 rupees amount should give 23 coins');
+  test.assertEquality(1, vendingMachine.dispenseCoins(5), '5 rupees amount should give 1 coin');
+  test.assertEquality(9, vendingMachine.dispenseCoins(45) , '45 rupees amount should give 9 coins');
+  test.assertEquality(22, vendingMachine.dispenseCoins(103) , '103 rupees amount should give 22 coins');
 }
 
 testVendingMachine();
