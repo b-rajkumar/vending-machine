7efa86fd29e986c233bdc907315396c900e48ef1
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-04-13 22:53:16 +0530

    Improved test messages and removed unnecessary tests

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,53 +4,40 @@ const assertListsEqual = test.assertListsEqual;
 const assertObjectsEqual = test.assertObjectsEqual;
 const assertEquality = test.assertEquality;
 const headLine = test.headLine;
-const maxSort = vendingMachine.maxSort;
-const computeCountByDenominations = vendingMachine.computeCountByDenominations;
-const computeCoinCount = vendingMachine.computeCoinCount;
+const coinsOfDenominations = vendingMachine.coinsOfDenominations;
+const vendCoins = vendingMachine.vendCoins;
 
+const testOrderedDenominations = function() {
+  headLine('Testing vendCoins function for ordered denominations');
 
-const testMaxSort = function() {
-  headLine('Testing maxSort function');
-
-  assertListsEqual([5, 2, 1], maxSort([1, 5, 2]),'max sort should give 5, 2, 1 for 1, 5, 2 input');
-  assertListsEqual([10, 5, 2, 1], maxSort([10, 1, 5, 2]),'max sort should give 10, 5, 2, 1 for 10, 1, 5, 2 input');
-  assertListsEqual([5, 2, 1], maxSort([5, 2, 1]),'max sort should give 5, 2, 1 for 5, 2, 1 input');
-
-  console.log();
-}
-
-const testVendingMachine = function() {
-  headLine('Testing computeCoinCount function');
-
-  assertEquality(0, computeCoinCount(10, []),'10 Rs and 0 denominations should give 0');
-  assertEquality(0, computeCoinCount(1, [5]),'1 rupee and 5 as denominations should give 0');
-  assertEquality(3, computeCoinCount(5, [1, 2]),'5 Rs and 1, 2 as denominations should give 3 coin');
-  assertEquality(1, computeCoinCount(10, [10]),'10 Rs and 10 as denominations should give 1 coin');
-  assertEquality(12, computeCoinCount(103, [1, 2, 5, 10]), '103 Rs and 1, 2, 5, 10 as denominations should give 12 coins');
-  assertEquality(4, computeCoinCount(13, [1, 4, 7]),'13 Rs and 1, 4, 7 as denominations should give 4 coins');
+  assertEquality(0, vendCoins(10, []),'Testing for zero denominations should give 0');
+  assertEquality(0, vendCoins(1, [5]),'Testing for amount less than denominations should give 0');
+  assertEquality(1, vendCoins(10, [10]),'Testing for denomination value equal to amount should give 1 coin');
+  assertEquality(12, vendCoins(103, [1, 2, 5, 10]), 'Testing for a set of denominations should give optimum coins');
 
   console.log();
 }
 
-const testForUnorderedDenominations = function() {
-  headLine('Testing computeCoinCount function for unordered denominations');
-  assertEquality(4, computeCoinCount(13, [4, 1, 7]),'13 Rs and 4, 1, 7 as denominations should give 4 coins');
-  assertEquality(12, computeCoinCount(107, [2, 10, 5, 1]),'107 Rs and 2, 10, 5, 1 as denominations should give 12 coins');
+const testUnorderedDenominations = function() {
+  headLine('Testing vendCoins function for unordered denominations');
+
+  assertEquality(0, vendCoins(1, []),'Testing for no denominations should give 0');
+  assertEquality(4, vendCoins(13, [4, 1, 7]),'Testing for unordered set of denominations');
+
   console.log();
 }
 
-const testCoinsByDenominations = function() {
-  headLine('Testing Coins function');
-  assertObjectsEqual({10:4, 1:5}, computeCountByDenominations(45, [1, 10]),'45 Rs and 1, 10 as denominations should give 10:4, 1:5 computeCountByDenominations');
-  assertObjectsEqual({7:1, 1:2, 4:1}, computeCountByDenominations(13, [4, 1, 7]),'13 Rs and 4, 1, 7 as denominations should give 7:1, 4:1, 1:2 computeCountByDenominations');
-  assertObjectsEqual({10:10, 2:1, 1:1}, computeCountByDenominations(103, [1, 2, 5, 10]),'103 Rs and 1, 2, 5, 10 as denominations should give 10:10, 5:0, 2:1, 1:1 computeCountByDenominations');
+const testCoinsByDenominationsFn = function() {
+  headLine('Testing CoinsByDenominations function');
+  assertObjectsEqual({}, coinsOfDenominations(45, []),'Testing for 0 denomination should give nothing');
+  assertObjectsEqual({5:9}, coinsOfDenominations(45, [5]),'Testing for one denomination should give the coin count');
+  assertObjectsEqual({7:1, 1:2, 4:1}, coinsOfDenominations(13, [4, 1, 7]),'Testing for a set of denominations should give coin count of each denomination');
 }
 
 const runTests = function() {
-  testMaxSort();
-  testVendingMachine();
-  testForUnorderedDenominations();
-  testCoinsByDenominations();
+  testOrderedDenominations();
+  testUnorderedDenominations();
+  testCoinsByDenominationsFn();
   test.displaySummary();
 }
 
